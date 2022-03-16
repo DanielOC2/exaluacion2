14c7123df0ba8618eb809faa43b58cde18960ad7
Elisabeth <elivelasquez1@misena.edu.co>
2022-03-16 14:53:59 -0500

    Agregar archivos de proyecto.

---
+++ exaluacion2/Cuenta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exaluacion2
+{
+    internal class Cuenta
+    {
+        public int numeroCuenta { get; set; }
+        public int documento { get; set; }
+        public int saldoActual { get; set; }
+        public int interesAnual { get; set; }
+        public string actualizarSaldo { get; set; }
+        public double ingresar { get; set; }
+        public double retirar { get; set; }
+
+        Cuenta(int numeroCuenta, int documento, int saldo, int interesAnual)
+        {
+            numeroCuenta = 198765432;
+            documento = 1001686125;
+            saldo = 10000;
+            interesAnual = 2;
+
+
+        }
+
+        public Cuenta()
+        {
+        }
+    }
+}
---
+++ exaluacion2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exaluacion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta = new();
+
+            Console.WriteLine("administrar cuenta bancaria");
+            Console.WriteLine("\n 1. actualizar saldo");          
+            Console.WriteLine("\n 2. ingresar dinero");           
+            Console.WriteLine("\n 3. retirar dinero");           
+            Console.WriteLine("\n 4. mostrar informacion de cuenta");
+            string menuBanco = Console.ReadLine();
+            
+
+            if (menuBanco == "si")
+            {
+
+            }
+            
+            
+
+           
+        }
+
+        
+
+        
+    }
+}
